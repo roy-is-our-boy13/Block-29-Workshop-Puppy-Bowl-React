f06c10a1112bc00d8b42669cb8b74a9254870601
Roy <rjperlman@gmail.com>
2024-12-09 13:30:13 -0800

    Filling in some more code.

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -14,18 +14,66 @@ const NewPlayerForm = () =>
 
         try
         {
+            const newPlayer = 
+            {
+                name: playerName,
+                breed: playerBreed,
+                photo: playerPhoto,
+                status: playerStatus,
+            };
+
+            console.log("New Player Added:", newPlayer);
+
+            fetchAllPlayers();
+
+            setPlayerName("");
+            setPlayerBreed("");
+            setPlayerPhoto("");
+            setPlayerStatus("bench");
 
         }
         catch(error)
         {
-            
+            console.error(error);
         }
 
     };
     
     return(
         <>
-      
+            <div>
+                <h2>New Player Form</h2>
+                <form id="new-player-form" onSubmit={inputPlayer}>
+                    <input
+                        type="text"
+                        id="nameOfThePlayer"
+                        placeholder="Player Name"
+                        value={playerName}
+                        onChange={(e) => setPlayerName(e.target.value)} required/>
+                    <input
+                        type="text"
+                        id="breedOfThePlayer"
+                        placeholder="Player Breed"
+                        value={playerBreed}
+                        onChange={(e) => setPlayerBreed(e.target.value)} required/>
+                    <input
+                        type="url"
+                        id="photoOfThePlayer"
+                        placeholder="Photo URL"
+                        value={playerPhoto}
+                        onChange={(e) => setPlayerPhoto(e.target.value)} required/>
+                    <select
+                        id="statusOfThePlayer"
+                        value={playerStatus}
+                        onChange={(e) => setPlayerStatus(e.target.value)}>
+                        <option value="bench">Bench</option>
+                        <option value="field">Field</option>
+                    </select>
+                    <button className="blackButton" type="submit">
+                        Add Player
+                    </button>
+                </form>
+            </div>
         </>
     );
   };
